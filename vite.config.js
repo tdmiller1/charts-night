13b70fd1d2726759d02be29f482952eb2a6720f9
tdmiller1 <tuckerdanielmiller@gmail.com>
2025-05-31 22:25:41 -0400

    Remove base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/charts-night/', // Set base for GitHub Pages project site
+  base: '/', // Set base to root for Render static site deployment
   plugins: [react()],
 });
